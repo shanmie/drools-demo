f5ca07ebd23024866eaa5b386ee2c18fd6a8e75c
miemie <jasonpe@163.com>
2019-09-27 23:33:46 +0800

    o

---
+++ src/main/java/com/example/drools/Tag.java
@@ -0,0 +1,14 @@
+package com.example.drools;
+
+import lombok.Data;
+
+@Data
+public class Tag {
+    private int uid;
+    private String country;
+    private String 出发口岸;
+    private String sex;
+    private String 是否带孩子;
+    private int 决策周期;
+    private String location;
+}
